12e6bcd6bc3c43e6d01836ca4e86bcd4830c5446
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-30 17:27:01 -0500

    set up login

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login'
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      logged: false,
+      username: ''
+    }
+  }
+  handleLogin = (username, logged) => {
+    this.setState({
+      username: username,
+      logged: logged
+    })
+  }
   render() {
     return (
       <div className="App">
--- pokemon-app/src/Login/index.js
+++ pokemon-app/src/Login/index.js
@@ -9,11 +9,24 @@ class Login extends Component {
 			password: ''
 		}
 	}
+	handleInput = (e) => {
+		this.setState({
+			[e.currentTarget.name]: e.currentTarget.value
+		})
+	}
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.handleLogin(this.state.username, true);
+	}
     render(){
         return(
         	<form onSubmit={this.handleSubmit}>
-            
+	        	 <input type="text" name="username"value={this.state.username}placeholder="username"/>
+	        	 <input type="text" name="password"value={this.state.password}placeholder="password"/>
+	        	 <input type="submit" value="Login"/>
+        	</form>
         )
     }
 }
 export default Login;
+
