9a2cb1967186a4980b88c3b90f9eeea23354bfa4
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-30 17:40:23 -0500

    started pokemon API request in main container

--- pokemon-app/src/MainContainer/index.js
+++ pokemon-app/src/MainContainer/index.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 
 
 class MainContainer extends Component {
+	constructor(){
+		super();
+		this.state = {
+			pokemon: []
+		}
+	}
+	getPokemon = async () => {
+		try {
+			const pokemon = await fetch('')
+			const pokemonJson = await pokemon.json();
+			return pokemonJson
+		} catch (err) {
+			return err
+		}
+	}
     render(){
         return(
             <h1>Pokemon</h1>
