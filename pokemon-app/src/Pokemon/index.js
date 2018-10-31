8a1d17a90b7dffb1fdfc8423871e38cff27b4590
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-31 10:18:48 -0500

    added moves of the pokemon

--- pokemon-app/src/Pokemon/index.js
+++ pokemon-app/src/Pokemon/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Form, Grid, Segment } from 'semantic-ui-react';
+import { Button, Form, Grid, Segment, Image } from 'semantic-ui-react';
 
 class Pokemon extends Component {
 
@@ -9,28 +9,29 @@ class Pokemon extends Component {
 
     		return 	(
     			
-    			<Grid container columns={1} textAlign='center' verticalAlign='middle' style={{height: '100%'}}>
+    		<Grid container columns={1} textAlign='center' verticalAlign='middle' style={{height: '100%'}}>
     			<Grid.Column style={{maxWidth: 450}} >
-    			<Segment color="blue">
+    			 <Segment color="blue">
     			<li key={i}> Name: {pokes.name}<br/>
-    			<img src={pokes.sprites.front_default}/><br/>
-    			Height: {pokes.height}<br/>
-    			Weight: {pokes.weight}<br/><br/>
-
-    		 </li>
+    				<img src={pokes.sprites.front_default}/><br/>
+    				Height: {pokes.height}<br/>
+    				Weight: {pokes.weight}<br/>
+    				Moves: {pokes.moves[0].move.name}<br/>
+    		 	</li>
     		 </Segment>
-    		 </Grid.Column>
-    		 </Grid>
+    		</Grid.Column>
+    	</Grid>
     		 )
     	
     	});
-
         return(
         	<div>
-             <h1 verticalAlign='middle'textAlign="center">Pokemon</h1>
+             <h1>Pokemon</h1>
              <ul>{pokemonList}</ul>
             </div>
         )
     }
 }
 export default Pokemon;
+
+
