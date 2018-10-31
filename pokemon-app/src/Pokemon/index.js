3d18326fbb49af5e379affeddd6a6750a493555b
Josh Hepworth <jrhepworth12@gmail.com>
2018-10-31 09:57:55 -0500

    styled the pokemon page

--- pokemon-app/src/Pokemon/index.js
+++ pokemon-app/src/Pokemon/index.js
@@ -7,20 +7,28 @@ class Pokemon extends Component {
     	console.log(this.props.pokemon)
     	const pokemonList = this.props.pokemon.map((pokes, i) => {
 
-    		return 	<li key={i}> Name: {pokes.name}<br/>
-    							<img src={pokes.sprites.front_default}/><br/>
-    							<img src={pokes.sprites.back_default}/><br/>
-    							Height: {pokes.height}<br/>
-    							Weight: {pokes.weight}<br/><br/>
-    		 </li>
+    		return 	(
+    			
+    			<Grid container columns={1} textAlign='center' verticalAlign='middle' style={{height: '100%'}}>
+    			<Grid.Column style={{maxWidth: 450}} >
+    			<Segment color="blue">
+    			<li key={i}> Name: {pokes.name}<br/>
+    			<img src={pokes.sprites.front_default}/><br/>
+    			Height: {pokes.height}<br/>
+    			Weight: {pokes.weight}<br/><br/>
 
+    		 </li>
+    		 </Segment>
+    		 </Grid.Column>
+    		 </Grid>
+    		 )
     	
     	});
 
         return(
         	<div>
-            <h1>Pokemon</h1>
-            <ul>{pokemonList}</ul>
+             <h1 verticalAlign='middle'textAlign="center">Pokemon</h1>
+             <ul>{pokemonList}</ul>
             </div>
         )
     }
